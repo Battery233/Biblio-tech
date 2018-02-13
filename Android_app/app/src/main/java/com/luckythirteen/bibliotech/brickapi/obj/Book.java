9d3d998736453d8182616bc6c1d2422d87b17d73
Colin Parrott <s1546623@sms.ed.ac.uk>
2018-02-13 11:08:02 +0000

    Added Book class in app

---
+++ Android_app/app/src/main/java/com/luckythirteen/bibliotech/brickapi/obj/Book.java
@@ -0,0 +1,30 @@
+package com.luckythirteen.bibliotech.brickapi.obj;
+
+
+public class Book
+{
+    private String ISBN;
+    private String title;
+    private String author;
+
+    public Book(String ISBN, String title, String author)
+    {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+
+}
