d823c00e25c15285aaf19f6a5fcfb5a6ce850bd9
ColinParrott <s1546623@sms.ed.ac.uk>
2018-02-15 16:53:36 +0000

    Fixed MAC address

--- Android_app/app/src/main/java/com/luckythirteen/bibliotech/dev/DevActivity.java
+++ Android_app/app/src/main/java/com/luckythirteen/bibliotech/dev/DevActivity.java
@@ -38,9 +38,9 @@ public class DevActivity extends AppCompatActivity {
     private static final String TAG = "DevActivity";
 
     // MAC addresses
-    public static final String TARGET_MAC = "AC:FD:CE:2B:82:F1";                                   // Colin's laptop
+    // public static final String TARGET_MAC = "AC:FD:CE:2B:82:F1";                                   // Colin's laptop
     // public static final String TARGET_MAC = "78:0c:b8:0b:a0:44";                                   // Chenghao's laptop
-    // public static final String TARGET_MAC = "B0:B4:48:76:E7:86";                                      // EV3 33
+    public static final String TARGET_MAC = "B0:B4:48:76:E7:86";                                      // EV3 33
 
     // UI elements
     private TextView bluetoothStatus;
