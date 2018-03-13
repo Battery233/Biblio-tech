7e256d5b9b84d8ad0c2f72a8a23d8ca528b197d7
Leonardo Mazzone <hello@leomazzone.me>
2018-03-13 18:39:29 +0000

    Add script to test vertical mov

---
+++ test_vertical.py
@@ -0,0 +1,11 @@
+# IMPORTANT: TO BE RUN FROM THE MAIN BRICK
+
+import control
+import time
+
+
+if __name__ == '__main__':
+    # Initialize robot, starts listening for commands
+    robot = control.Controller('ev3_main')
+    time.sleep(5)
+    robot.server.send_to_device("up", ev3_server.Device.OTHER_EV3)
