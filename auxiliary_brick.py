6f97e4479f51f6ed0d38c0a9d464f0d324540bd3
Battery233 <Battery233@users.noreply.github.com>
2018-03-13 15:46:30 +0000

    fixed a function name bug

--- auxiliary_brick.py
+++ auxiliary_brick.py
@@ -1,11 +1,11 @@
 import control
 
+
 class AuxController(control.Controller):
     def __init__(self, server_name):
         super().__init__(server_name)
 
 
-
 if __name__ == '__main__':
     # Initialize auxiliary brick, starts listening for commands
-    brick = MainController('ev3_aux')
+    brick = AuxController('ev3_aux')
