3595f81930ce26a50b6d5f101a8f099c02a7b846
Battery233 <Battery233@users.noreply.github.com>
2018-03-30 18:07:18 +0100

    fix bug: make all db commands string

--- db/commands.py
+++ db/commands.py
@@ -23,11 +23,11 @@ def command_select_all():
 
 
 def command_update_book_position(ISBN, position):
-    return "UPDATE %s SET position='%s' WHERE ISBN= %d" % (TABLE_NAME, position, ISBN)
+    return "UPDATE %s SET position='%s' WHERE ISBN= %s" % (TABLE_NAME, position, ISBN)
 
 
 def command_update_book_status(ISBN, status):
-    return "UPDATE %s SET status='%s' WHERE ISBN=%d" % (TABLE_NAME, status, ISBN)
+    return "UPDATE %s SET status='%s' WHERE ISBN=%s" % (TABLE_NAME, status, ISBN)
 
 
 def command_get_all_titles_and_positions():
@@ -39,7 +39,7 @@ def command_get_all_ISBNs():
 
 
 def command_get_position_by_ISBN(isbn):
-    return "SELECT position FROM %s WHERE ISBN=%d" % (TABLE_NAME, isbn)
+    return "SELECT position FROM %s WHERE ISBN=%s" % (TABLE_NAME, isbn)
 
 
 def command_get_position_by_title(title):
@@ -48,29 +48,3 @@ def command_get_position_by_title(title):
 
 def command_get_ISBN_by_title(title):
     return "SELECT ISBN FROM %s WHERE title=%s" % (TABLE_NAME, title)
-
-
-# *******************************************************************************#
-# Following are some output example cases to make sure the whole thing is right  #
-# *******************************************************************************#
-STATUS_UNAVAILABLE = '0'
-STATUS_AVAILABLE = '1'
-print(command_create())
-print('''CREATE TABLE books
-(ISBN INTEGER, title TEXT, author TEXT, position TEXT,
-status INTEGER, UNIQUE(ISBN) ON CONFLICT REPLACE)''')
-print("DELETE FROM books")
-print(command_delete())
-print("INSERT INTO books VALUES (9780140441185, 'Thus Spoke Zarathustra', 'Friedrich Nietzsche', '2'," +
-      STATUS_UNAVAILABLE + ")")
-print(command_add_item('9780140441185', 'Thus Spoke Zarathustra',
-                       'Friedrich Nietzsche', '2', STATUS_UNAVAILABLE))
-print("SELECT * FROM books")
-print(command_select_all())
-print(command_update_book_position(123, "2"))
-print(command_update_book_status(123, "2"))
-print(command_get_all_titles_and_positions())
-print(command_get_all_ISBNs())
-print(command_get_position_by_ISBN(132))
-print(command_get_position_by_title("abc"))
-print(command_get_ISBN_by_title("abc"))
