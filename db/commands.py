549d893d3c45526ce03487f2175e0b787276ffc9
Colin <s1546623@sms.ed.ac.uk>
2018-03-30 18:15:07 +0100

    hopefully finalize db

--- db/commands.py
+++ db/commands.py
@@ -49,28 +49,3 @@ def command_get_position_by_title(title):
 def command_get_ISBN_by_title(title):
     return "SELECT ISBN FROM %s WHERE title=%s" % (TABLE_NAME, title)
 
-
-# *******************************************************************************#
-# Following are some output example cases to make sure the whole thing is right  #
-# *******************************************************************************#
-STATUS_UNAVAILABLE = '0'
-STATUS_AVAILABLE = '1'
-print(command_create())
-print('''CREATE TABLE books
-(ISBN INTEGER, title TEXT, author TEXT, position TEXT,
-status INTEGER, UNIQUE(ISBN) ON CONFLICT REPLACE)''')
-print("DELETE FROM books")
-print(command_delete())
-print("INSERT INTO books VALUES (9780140441185, 'Thus Spoke Zarathustra', 'Friedrich Nietzsche', '2'," +
-      STATUS_UNAVAILABLE + ")")
-print(command_add_item('9780140441185', 'Thus Spoke Zarathustra',
-                       'Friedrich Nietzsche', '2', STATUS_UNAVAILABLE))
-print("SELECT * FROM books")
-print(command_select_all())
-print(command_update_book_position(123, "2"))
-print(command_update_book_status(123, "2"))
-print(command_get_all_titles_and_positions())
-print(command_get_all_ISBNs())
-print(command_get_position_by_ISBN(132))
-print(command_get_position_by_title("abc"))
-print(command_get_ISBN_by_title("abc"))
